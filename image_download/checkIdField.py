893218017651baf21b4bfa80147903407f9e021b
Alex Burzinski <aburzinski2@gmail.com>
2019-04-30 12:12:01 -0500

    Adding script to download album images as jpg files

---
+++ image_download/checkIdField.py
@@ -0,0 +1,35 @@
+import os
+import json
+import requests
+
+def tryCastInt(val):
+    try: 
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+def checkDups(lst):
+    seen = set()
+    for x in lst:
+        if x in seen: return True
+        seen.add(x)
+    return False
+
+dataPath = os.path.join(os.sep, 'team', 'scratch', 'album-ai', 'data')
+albumFile = 'albums.jl'
+albumCoverFolder = 'albumArt'
+
+albumIds = []
+with open(os.path.join(dataPath, albumFile)) as f:
+    for album in f:
+
+        albumJson = json.loads(album)
+        albumId = albumJson['url'].split('/')[4].split('-')[0]
+        if not tryCastInt(albumId):
+            raise ValueError('albumId not cost to int')
+        albumIds.append(albumId)
+f.close()
+
+if checkDups(albumIds):
+    raise Exception('Duplicates found')
---
+++ image_download/downloadImages.py
@@ -0,0 +1,45 @@
+import os
+import json
+import requests
+
+dataPath = os.path.join(os.sep, 'team', 'scratch', 'album-ai', 'data')
+albumFile = 'albums.jl'
+albumCoverFolder = 'albumArt'
+
+numberAlbums = 5
+albumsRead = 0
+
+with open(os.path.join(dataPath, albumFile)) as f:
+    for album in f:
+        # if albumsRead >= numberAlbums:
+        #     break
+
+        albumJson = json.loads(album)
+        albumId = albumJson['url'].split('/')[4].split('-')[0]
+        albumName = albumJson['album']
+        artistName = albumJson['artist']
+        imageLink = albumJson['image_link']
+        
+        """ Need to decide how to do genre """
+        # Taking first genre for now
+        genres = albumJson['genres']
+        if len(genres) == 0:
+            genre = 'None'
+        else:
+            genre = genres[0]
+
+        """ Need to check to see if albumId will work """
+        # Including artistName, albumName, and genre can result in invalid file names
+        # imageName = albumId + '~' + artistName + '~' + albumName + '~' + genre
+        imageName = albumId
+        imageName += '.jpg'
+
+        with open(os.path.join(dataPath, albumCoverFolder, imageName), 'wb') as image:
+            response = requests.get(imageLink)
+            if not response.ok:
+                raise Exception(response.content)
+            image.write(response.content)
+        image.close()
+
+        albumsRead += 1
+f.close()
